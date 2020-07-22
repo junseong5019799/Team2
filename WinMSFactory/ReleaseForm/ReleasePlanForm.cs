40603b42f93b644102b8bc4d259456349f8ac7ac
노희재 <heejae0414@naver.com>
2020-07-22 18:22:55 +0900

    OrderForm 수정

--- WinMSFactory/ReleaseForm/ReleasePlanForm.cs
+++ WinMSFactory/ReleaseForm/ReleasePlanForm.cs
@@ -16,5 +16,21 @@ namespace WinMSFactory
         {
             InitializeComponent();
         }
+
+        private void ReleasePlanForm_Load(object sender, EventArgs e)
+        {
+            dgv.AddNewColumns("출고예정 번호", "", 100, true);
+            dgv.AddNewColumns("거래처", "", 100, true);
+            dgv.AddNewColumns("거래처명", "", 100, true);
+            dgv.AddNewColumns("출고 요청일", "", 100, true);
+            dgv.AddNewColumns("품목 코드", "", 100, true);
+            dgv.AddNewColumns("요청 수량", "", 100, true);
+            dgv.AddNewColumns("출고 상태", "", 100, true);
+            dgv.AddNewColumns("출고일", "", 100, true);
+            dgv.AddNewColumns("최초등록 시각", "", 100, true);
+            dgv.AddNewColumns("최종등록 시각", "", 100, true);
+            dgv.AddNewColumns("최초등록 시각", "", 100, true);
+            dgv.AddNewColumns("최종등록 사원", "", 100, true);
+        }
     }
 }
