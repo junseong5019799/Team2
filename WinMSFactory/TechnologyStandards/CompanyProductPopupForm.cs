7a266938edac992b4ab5283f0b127715845cfdd9
wnstjd5013@gmail.com <wnstjd5013@gmail.com>
2020-07-24 18:15:35 +0900

    수정

--- WinMSFactory/TechnologyStandards/CompanyProductPopupForm.cs
+++ WinMSFactory/TechnologyStandards/CompanyProductPopupForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WinMSFactory.TechnologyStandards
 {
-    public partial class CompanyProductPopupForm : Form
+    public partial class CompanyProductPopupForm : PopUpDialogForm
     {
         public CompanyProductPopupForm()
         {
