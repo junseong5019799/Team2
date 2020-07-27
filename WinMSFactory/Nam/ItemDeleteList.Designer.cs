b0f27a32d7c08e7792661b1a5b04c7e6c663055b
남기문 <skarl8053@naver.com>
2020-07-27 10:27:32 +0900

    BasicDataGridViewControl 추가

--- WinMSFactory/Nam/ItemDeleteList.Designer.cs
+++ WinMSFactory/Nam/ItemDeleteList.Designer.cs
@@ -34,13 +34,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.buttonControl1 = new WinMSFactory.ButtonControl();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UpList
             // 
             this.UpList.FormattingEnabled = true;
             this.UpList.ItemHeight = 12;
-            this.UpList.Location = new System.Drawing.Point(21, 86);
+            this.UpList.Location = new System.Drawing.Point(21, 106);
             this.UpList.Name = "UpList";
             this.UpList.Size = new System.Drawing.Size(341, 172);
             this.UpList.TabIndex = 0;
@@ -49,7 +50,7 @@
             // 
             this.DownList.FormattingEnabled = true;
             this.DownList.ItemHeight = 12;
-            this.DownList.Location = new System.Drawing.Point(21, 291);
+            this.DownList.Location = new System.Drawing.Point(21, 311);
             this.DownList.Name = "DownList";
             this.DownList.Size = new System.Drawing.Size(341, 172);
             this.DownList.TabIndex = 0;
@@ -58,7 +59,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 13F);
-            this.label1.Location = new System.Drawing.Point(30, 21);
+            this.label1.Location = new System.Drawing.Point(30, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(209, 18);
             this.label1.TabIndex = 1;
@@ -69,7 +70,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 10F);
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(30, 65);
+            this.label2.Location = new System.Drawing.Point(30, 85);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(107, 14);
             this.label2.TabIndex = 1;
@@ -80,7 +81,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("굴림", 10F);
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(30, 268);
+            this.label3.Location = new System.Drawing.Point(30, 288);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(107, 14);
             this.label3.TabIndex = 1;
@@ -90,7 +91,7 @@
             // 
             this.buttonControl1.BackColor = System.Drawing.Color.White;
             this.buttonControl1.ForeColor = System.Drawing.Color.Black;
-            this.buttonControl1.Location = new System.Drawing.Point(270, 469);
+            this.buttonControl1.Location = new System.Drawing.Point(270, 489);
             this.buttonControl1.Name = "buttonControl1";
             this.buttonControl1.Size = new System.Drawing.Size(82, 31);
             this.buttonControl1.TabIndex = 2;
@@ -98,15 +99,26 @@
             this.buttonControl1.UseVisualStyleBackColor = false;
             this.buttonControl1.Click += new System.EventHandler(this.buttonControl1_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 13F);
+            this.label4.Location = new System.Drawing.Point(30, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "제품이 삭제되었습니다.";
+            // 
             // ItemDeleteList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(383, 512);
+            this.ClientSize = new System.Drawing.Size(383, 532);
             this.Controls.Add(this.buttonControl1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DownList);
             this.Controls.Add(this.UpList);
@@ -128,5 +140,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private ButtonControl buttonControl1;
+        private System.Windows.Forms.Label label4;
     }
 }
