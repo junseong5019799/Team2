1f4785294d2fb8f390f459d8a5ad57170b11dc03
준성 김 <wnstjd5013@naver.com>
2020-08-09 20:20:30 +0900

    커밋

---
+++ MSFactoryDAC/DowntimeTypeDAC.cs
@@ -0,0 +1,113 @@
+﻿using MSFactoryVO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSFactoryDAC
+{
+    public class DowntimeTypeDAC:SqlHelper
+    {
+        public DataTable GetAll()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.ConnectionString))
+                {
+                    conn.Open();
+                    string sql = @"select [downtime_type_id], [downtime_type_name], [downtime_type_calculation], [downtime_type_seq], [downtime_type_use], [first_regist_time], [first_regist_employee], [final_regist_time], [final_regist_employee]
+                                     from [dbo].[TBL_DOWNTIME_TYPE]
+                                 Order by downtime_type_seq";
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    da.Fill(dt);
+
+                    return dt;
+                }
+
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
+
+        public bool UseTypeChange(int downtime_type_id, string isTypeUse = null, string isTimeuse = null)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = new SqlConnection(this.ConnectionString);
+                    cmd.Connection.Open();
+
+                    if (isTimeuse == null)
+                    {
+                        cmd.CommandText = @"UPDATE TBL_DOWNTIME_TYPE 
+                                        SET downtime_type_use = @downtime_type_use
+                                    WHERE downtime_type_id = @downtime_type_id";
+                            cmd.Parameters.AddWithValue("@downtime_type_use", isTypeUse);
+                    }
+                    else if(isTypeUse == null)
+                    {
+                        cmd.CommandText = @"UPDATE TBL_DOWNTIME_TYPE 
+                                        SET downtime_type_calculation  = @downtime_type_calculation
+                                    WHERE downtime_type_id = @downtime_type_id";
+                        cmd.Parameters.AddWithValue("@downtime_type_calculation", isTimeuse);
+                    }
+
+                    cmd.Parameters.AddWithValue("@downtime_type_id", downtime_type_id);
+
+                    if (cmd.ExecuteNonQuery() > 0)
+                        return true;
+
+                    else
+                        return false;
+                }
+                
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
+
+        public bool SaveDowntimeType(DowntimeTypeVO vo)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.ConnectionString))
+                {
+                    conn.Open();
+
+                    string sql = "SP_SAVE_DOWNTIMETYPE";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@P_downtime_type_id", vo.downtime_type_id);
+                        cmd.Parameters.AddWithValue("@P_downtime_type_name", vo.downtime_type_name);
+                        cmd.Parameters.AddWithValue("@P_downtime_type_calculation", vo.downtime_type_calculation);
+                        cmd.Parameters.AddWithValue("@P_downtime_type_seq", vo.downtime_type_seq);
+                        cmd.Parameters.AddWithValue("@P_downtime_type_use", vo.downtime_type_use);
+                        cmd.Parameters.AddWithValue("@P_first_regist_employee", vo.first_regist_employee);
+                        cmd.Parameters.AddWithValue("@P_final_regist_employee", vo.final_regist_employee);
+                       
+                        cmd.ExecuteNonQuery();
+                    }
+                    return true;
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
+
+    }
+}
--- WinMSFactory/Process/CorporationForm.Designer.cs
+++ WinMSFactory/Process/CorporationForm.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.txtNameSearch = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnClear = new WinMSFactory.ButtonControl();
@@ -159,23 +159,23 @@
             this.dgvCorporationlist.AllowUserToAddRows = false;
             this.dgvCorporationlist.BackgroundColor = System.Drawing.Color.White;
             this.dgvCorporationlist.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvCorporationlist.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCorporationlist.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvCorporationlist.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvCorporationlist.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvCorporationlist.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvCorporationlist.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvCorporationlist.IsAllCheckColumnHeader = false;
             this.dgvCorporationlist.IsAutoGenerateColumns = false;
@@ -198,7 +198,7 @@
             this.Controls.Add(this.panel2);
             this.Margin = new System.Windows.Forms.Padding(3);
             this.Name = "CorporationForm";
-            this.Text = "법인관리";
+            this.Text = "";
             this.Load += new System.EventHandler(this.CorporationForm_Load);
             this.Controls.SetChildIndex(this.panel1, 0);
             this.Controls.SetChildIndex(this.GuidLabel2, 0);
