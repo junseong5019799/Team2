be505096dba81dc6ed705560dbc79df0d8f6e795
남기문 <skarl8053@naver.com>
2020-08-11 09:53:23 +0900

    엑셀 유틸 수정중

--- WinMSFactory/Util/ExcelUtil.cs
+++ WinMSFactory/Util/ExcelUtil.cs
@@ -1,7 +1,4 @@
-﻿using DevExpress.Data.Access;
-using DevExpress.DataAccess.Native.Data;
-using DevExpress.DataAccess.Sql;
-using DevExpress.XtraEditors;
+﻿using MSFactoryDAC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,65 +7,54 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
 
 namespace WinMSFactory.Util
 {
     public class ExcelUtil
     {
-        
-        public static void ListToExcel<T>(List<T> dataList) // string[] Columns
+        public ExcelUtil() 
         {
-            List<string> ColumnList = new List<string>(); // 컬럼 리스트
-
+        }
+        public static void ListToExcel<T>(string[] ColumnList, List<T> dataList) // string[] Columns
+        {
             SaveFileDialog dialog = new SaveFileDialog();
 
-            Type type = typeof(T);
-
-            foreach (PropertyInfo property in type.GetProperties())
-                ColumnList.Add(property.Name);
-
-            try
+            dialog.Filter = "Excel 파일(*.xlsx)|*.xlsx";
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
-                dialog.Filter = "Excel 파일(*.xlsx)|*.xlsx";
-                
-
-                if (dialog.ShowDialog() == DialogResult.OK)
+                try
                 {
-                    try
-                    {
-                        Excel.Application xlApp = new Excel.Application();
-                        Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
-                        Excel.Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets.Add();
-                        xlApp.Visible = true;
+                    Excel.Application xlApp = new Excel.Application();
+                    Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
+                    Excel.Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets.Add();
+                    xlApp.Visible = true;
 
-                        for(int i = 1; i< dataList.Count; i++)// row
+                    for(int i = 1; i<ColumnList.Length; i++)
+                        xlWorksheet.Cells[0,i] = ColumnList[i].ToString();
+
+
+                    for (int i = 1; i< dataList.Count; i++)// row
+                    {
+                        for(int j = 1; j<ColumnList.Length; j++) // column
                         {
-                            for(int j = 1; j<ColumnList.Count; j++) // column
-                            {
-                                if (i == 1)
-                                    xlWorksheet.Cells[i, j] = ColumnList[j].ToString();
-
-                                else
-                                    xlWorksheet.Cells[i, j] = ColumnList[i][j].ToString();
-                            }
+                            xlWorksheet.Cells[i, j] = ColumnList[i][j].ToString();
                         }
-
-                        xlWorkbook.SaveAs(dialog.FileName);
-                        xlWorkbook.Close(true);
-                        xlApp.Quit();
-
-                        MessageBox.Show("엑셀이 생성되었습니다.");
                     }
-                    catch
-                    {
-                        MessageBox.Show("오류가 발생하였습니다. 다시 시도하여 주세요");
-                    }
+
+                    xlWorkbook.SaveAs(dialog.FileName);
+                    xlWorkbook.Close(true);
+                    xlApp.Quit();
+
+                    MessageBox.Show("엑셀이 생성되었습니다.");
+                }
+                catch
+                {
+                    MessageBox.Show("오류가 발생하였습니다. 다시 시도하여 주세요");
                 }
             }
-            catch
-            {
-
-            }
+           
 
         }
         public static void DataTableToExcel(DataTable dt)
@@ -76,11 +62,36 @@ namespace WinMSFactory.Util
 
             try
             {
+                SaveFileDialog dialog = new SaveFileDialog();
 
+                dialog.Filter = "Excel 파일(*.xlsx)|*.xlsx";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Excel.Application xlApp = new Excel.Application();
+                    Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
+                    Excel.Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets.Add();
+                    xlApp.Visible = true;
+
+                    for (int i = 1; i < dt.Columns.Count; i++)
+                        xlWorksheet.Cells[1, i] = dt.Rows[0][i].ToString();
+
+                    for(int i = 2; i<=dt.Rows.Count; i++)
+                    {
+                        for(int j = 1; j<=dt.Columns.Count; j++)
+                            xlWorksheet.Cells[j, i] = dt.Rows[i][j].ToString();
+                    }
+
+                    xlWorkbook.SaveAs(dialog.FileName);
+                    xlWorkbook.Close(true);
+                    xlApp.Quit();
+
+                    MessageBox.Show("엑셀이 생성되었습니다.");
+                }
             }
             catch
             {
-
+                MessageBox.Show("오류가 발생하였습니다. 다시 시도하여 주세요");
             }
         }
     }
