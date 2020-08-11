a74eff9c33cc1d783cf53661c8a71ea4281dee5f
wnstjd5013@gmail.com <wnstjd5013@gmail.com>
2020-08-11 18:26:05 +0900

    커밋

--- WinMSFactory/Technology_Standards/StorageForm.Designer.cs
+++ WinMSFactory/Technology_Standards/StorageForm.Designer.cs
@@ -28,10 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.buttonControl5 = new WinMSFactory.ButtonControl();
-            this.buttonControl4 = new WinMSFactory.ButtonControl();
             this.buttonControl3 = new WinMSFactory.ButtonControl();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonControl2 = new WinMSFactory.ButtonControl();
@@ -55,12 +54,10 @@
             this.panel1.Controls.Add(this.buttonControl2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.buttonControl5);
-            this.panel1.Controls.Add(this.buttonControl4);
             this.panel1.Controls.Add(this.buttonControl3);
-            this.panel1.Size = new System.Drawing.Size(1753, 170);
+            this.panel1.Size = new System.Drawing.Size(1753, 80);
             this.panel1.Controls.SetChildIndex(this.GuidLabel1, 0);
             this.panel1.Controls.SetChildIndex(this.buttonControl3, 0);
-            this.panel1.Controls.SetChildIndex(this.buttonControl4, 0);
             this.panel1.Controls.SetChildIndex(this.buttonControl5, 0);
             this.panel1.Controls.SetChildIndex(this.label1, 0);
             this.panel1.Controls.SetChildIndex(this.buttonControl2, 0);
@@ -69,6 +66,10 @@
             this.panel1.Controls.SetChildIndex(this.label2, 0);
             this.panel1.Controls.SetChildIndex(this.cboCorporation, 0);
             this.panel1.Controls.SetChildIndex(this.txtStorage, 0);
+            // 
+            // GuidLabel1
+            // 
+            this.GuidLabel1.Location = new System.Drawing.Point(11, 6);
             // 
             // GuidLabel2
             // 
@@ -79,30 +80,18 @@
             // 
             this.buttonControl5.BackColor = System.Drawing.Color.White;
             this.buttonControl5.ForeColor = System.Drawing.Color.Black;
-            this.buttonControl5.Location = new System.Drawing.Point(1296, 96);
+            this.buttonControl5.Location = new System.Drawing.Point(1201, 30);
             this.buttonControl5.Name = "buttonControl5";
             this.buttonControl5.Size = new System.Drawing.Size(55, 23);
             this.buttonControl5.TabIndex = 27;
             this.buttonControl5.Text = "초기화";
             this.buttonControl5.UseVisualStyleBackColor = false;
             // 
-            // buttonControl4
-            // 
-            this.buttonControl4.BackColor = System.Drawing.Color.White;
-            this.buttonControl4.ForeColor = System.Drawing.Color.Black;
-            this.buttonControl4.Location = new System.Drawing.Point(896, 96);
-            this.buttonControl4.Name = "buttonControl4";
-            this.buttonControl4.Size = new System.Drawing.Size(57, 23);
-            this.buttonControl4.TabIndex = 26;
-            this.buttonControl4.Text = "조회";
-            this.buttonControl4.UseVisualStyleBackColor = false;
-            this.buttonControl4.Click += new System.EventHandler(this.buttonControl4_Click);
-            // 
             // buttonControl3
             // 
             this.buttonControl3.BackColor = System.Drawing.Color.White;
             this.buttonControl3.ForeColor = System.Drawing.Color.Black;
-            this.buttonControl3.Location = new System.Drawing.Point(1167, 96);
+            this.buttonControl3.Location = new System.Drawing.Point(1115, 29);
             this.buttonControl3.Name = "buttonControl3";
             this.buttonControl3.Size = new System.Drawing.Size(46, 23);
             this.buttonControl3.TabIndex = 25;
@@ -113,7 +102,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(337, 83);
+            this.label1.Location = new System.Drawing.Point(299, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 16);
             this.label1.TabIndex = 28;
@@ -123,7 +112,7 @@
             // 
             this.buttonControl2.BackColor = System.Drawing.Color.White;
             this.buttonControl2.ForeColor = System.Drawing.Color.Black;
-            this.buttonControl2.Location = new System.Drawing.Point(1036, 96);
+            this.buttonControl2.Location = new System.Drawing.Point(1021, 29);
             this.buttonControl2.Name = "buttonControl2";
             this.buttonControl2.Size = new System.Drawing.Size(48, 23);
             this.buttonControl2.TabIndex = 32;
@@ -133,7 +122,7 @@
             // cboFactoryName
             // 
             this.cboFactoryName.FormattingEnabled = true;
-            this.cboFactoryName.Location = new System.Drawing.Point(408, 80);
+            this.cboFactoryName.Location = new System.Drawing.Point(370, 29);
             this.cboFactoryName.Name = "cboFactoryName";
             this.cboFactoryName.Size = new System.Drawing.Size(138, 24);
             this.cboFactoryName.TabIndex = 33;
@@ -143,39 +132,39 @@
             this.dgv.AllowUserToAddRows = false;
             this.dgv.BackgroundColor = System.Drawing.Color.White;
             this.dgv.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgv.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgv.IsAllCheckColumnHeader = false;
             this.dgv.IsAutoGenerateColumns = false;
-            this.dgv.Location = new System.Drawing.Point(0, 168);
+            this.dgv.Location = new System.Drawing.Point(0, 70);
             this.dgv.MultiSelect = false;
             this.dgv.Name = "dgv";
             this.dgv.RowHeadersVisible = false;
             this.dgv.RowTemplate.Height = 23;
             this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgv.Size = new System.Drawing.Size(1753, 846);
+            this.dgv.Size = new System.Drawing.Size(1753, 944);
             this.dgv.TabIndex = 4;
             this.dgv.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellDoubleClick);
             // 
             // cboCorporation
             // 
             this.cboCorporation.FormattingEnabled = true;
-            this.cboCorporation.Location = new System.Drawing.Point(163, 79);
+            this.cboCorporation.Location = new System.Drawing.Point(125, 29);
             this.cboCorporation.Name = "cboCorporation";
             this.cboCorporation.Size = new System.Drawing.Size(138, 24);
             this.cboCorporation.TabIndex = 35;
@@ -184,7 +173,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(92, 82);
+            this.label2.Location = new System.Drawing.Point(54, 33);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 16);
             this.label2.TabIndex = 34;
@@ -194,7 +183,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(581, 83);
+            this.label3.Location = new System.Drawing.Point(543, 33);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 16);
             this.label3.TabIndex = 28;
@@ -202,7 +191,7 @@
             // 
             // txtStorage
             // 
-            this.txtStorage.Location = new System.Drawing.Point(653, 82);
+            this.txtStorage.Location = new System.Drawing.Point(615, 30);
             this.txtStorage.Name = "txtStorage";
             this.txtStorage.Size = new System.Drawing.Size(208, 22);
             this.txtStorage.TabIndex = 36;
@@ -231,7 +220,6 @@
         #endregion
 
         private ButtonControl buttonControl5;
-        private ButtonControl buttonControl4;
         private ButtonControl buttonControl3;
         private System.Windows.Forms.Label label1;
         private ButtonControl buttonControl2;
---
+++ WinMSFactory/Technology_Standards/StoragePopupForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WinMSFactory
+{
+    partial class StoragePopupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCorporation = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoUnUse = new System.Windows.Forms.RadioButton();
+            this.rdoUse = new System.Windows.Forms.RadioButton();
+            this.cboFactory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStorage = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 410);
+            this.panel1.Size = new System.Drawing.Size(450, 40);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.btnConfirm.Location = new System.Drawing.Point(89, 4);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(246, 4);
+            // 
+            // cboCorporation
+            // 
+            this.cboCorporation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCorporation.FormattingEnabled = true;
+            this.cboCorporation.Location = new System.Drawing.Point(118, 47);
+            this.cboCorporation.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboCorporation.Name = "cboCorporation";
+            this.cboCorporation.Size = new System.Drawing.Size(263, 24);
+            this.cboCorporation.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoUnUse);
+            this.groupBox1.Controls.Add(this.rdoUse);
+            this.groupBox1.Location = new System.Drawing.Point(53, 210);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(329, 73);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "사용 여부";
+            // 
+            // rdoUnUse
+            // 
+            this.rdoUnUse.AutoSize = true;
+            this.rdoUnUse.Location = new System.Drawing.Point(202, 32);
+            this.rdoUnUse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rdoUnUse.Name = "rdoUnUse";
+            this.rdoUnUse.Size = new System.Drawing.Size(59, 20);
+            this.rdoUnUse.TabIndex = 0;
+            this.rdoUnUse.TabStop = true;
+            this.rdoUnUse.Text = "미사용";
+            this.rdoUnUse.UseVisualStyleBackColor = true;
+            // 
+            // rdoUse
+            // 
+            this.rdoUse.AutoSize = true;
+            this.rdoUse.Location = new System.Drawing.Point(63, 32);
+            this.rdoUse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rdoUse.Name = "rdoUse";
+            this.rdoUse.Size = new System.Drawing.Size(48, 20);
+            this.rdoUse.TabIndex = 0;
+            this.rdoUse.TabStop = true;
+            this.rdoUse.Text = "사용";
+            this.rdoUse.UseVisualStyleBackColor = true;
+            // 
+            // cboFactory
+            // 
+            this.cboFactory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFactory.FormattingEnabled = true;
+            this.cboFactory.Location = new System.Drawing.Point(118, 101);
+            this.cboFactory.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboFactory.Name = "cboFactory";
+            this.cboFactory.Size = new System.Drawing.Size(263, 24);
+            this.cboFactory.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "창고명칭";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "공장 명칭";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "법인명";
+            // 
+            // txtStorage
+            // 
+            this.txtStorage.Location = new System.Drawing.Point(118, 153);
+            this.txtStorage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtStorage.Name = "txtStorage";
+            this.txtStorage.Size = new System.Drawing.Size(263, 22);
+            this.txtStorage.TabIndex = 5;
+            // 
+            // StoragePopupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.cboCorporation);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cboFactory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStorage);
+            this.Name = "StoragePopupForm";
+            this.Text = "StoragePopupForm";
+            this.Load += new System.EventHandler(this.StoragePopupForm_Load);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.txtStorage, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.cboFactory, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.cboCorporation, 0);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCorporation;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoUnUse;
+        private System.Windows.Forms.RadioButton rdoUse;
+        private System.Windows.Forms.ComboBox cboFactory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStorage;
+    }
+}
