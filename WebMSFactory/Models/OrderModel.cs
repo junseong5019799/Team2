f1d3f8198ceb8da4944d8af9228c0cef51b7f4c0
남기문 <skarl8053@naver.com>
2020-08-19 15:20:44 +0900

    웹 진행중 및 자재단가관리 회사 목록 수정

--- WebMSFactory/Models/OrderModel.cs
+++ WebMSFactory/Models/OrderModel.cs
@@ -3,27 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace WebMSFactory.Models
+namespace WebMSFactory
 {
     public class OrderModel
    {
-		//SELECT od.order_no, product_name, od.product_id
-   
-		//	, od.order_request_quantity
-   
-		//	, od.order_status , od.order_request_date , Convert(DATE, o.first_regist_time) first_regist_time
-		//	, (SELECT company_name FROM TBL_COMPANY WHERE o.company_id = company_id) AS company_name
-		//	 , o.final_regist_employee, o.final_regist_time
-		//FROM TBL_ORDER_DETAIL od INNER JOIN TBL_ORDER o ON o.order_no = od.order_no
-		//						 INNER JOIN TBL_PRODUCT p ON od.product_id = p.product_id
-
-
-  //      public int Order_no { get; set; }
-		//public string Product_name { get; set; }
-		//public string order_request_quantity { get; set; }
-  //      public string order_status { get; set; }
-		//public DateTime order_request_date
-
-
-    }
+		public int Order_No { get; set; }
+		public string Product_Name { get; set; }
+		public string Orer_Request_Quantity_string { get; set; }
+		public string Order_Status { get; set; }
+		public DateTime Order_Request_Date { get; set; }
+		public string Company_Name { get; set; }
+	}
 }
