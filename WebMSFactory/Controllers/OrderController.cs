b9a05ec26131a216875198d33e6a5882fab29abc
남기문 <skarl8053@naver.com>
2020-08-19 18:19:45 +0900

    웹 진행중

--- WebMSFactory/Controllers/OrderController.cs
+++ WebMSFactory/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,11 +11,34 @@ namespace WebMSFactory.Controllers
     public class OrderController : Controller
     {
         // GET: Order
-        public ActionResult OrderList()
+        public ActionResult OrderList(string ProductName="")
         {
             OrderListDAC dac = new OrderListDAC();
 
-            return View(dac.AllOrderList());
+            var SortedList = (from sorted in dac.AllOrderList()
+                              where sorted.Product_Name.Contains(ProductName)
+                              select sorted).ToList();
+
+            OrderListViewModel Model = new OrderListViewModel
+            {
+                OrderLists = SortedList
+            };
+            
+
+            return View(Model);
+        }
+
+        public ActionResult ReleaseList(string ProductName = "")
+        {
+            OrderListDAC dac = new OrderListDAC();
+
+            var SortedList = (from sorted in dac.AllReleaseList()
+                              where sorted.Product_Name.Contains(ProductName)
+                              select sorted).ToList();
+
+            OrderListViewModel Model = new OrderListViewModel();
+            Model.OrderLists = SortedList;
+            return View(Model);
         }
     }
 }
